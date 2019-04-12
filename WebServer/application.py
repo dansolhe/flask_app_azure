95ef7eadd33282dfa7e416bc8e0205d8658f9092
Daniel A. Solheim <dansolhe@gmail.com>
2019-04-12 13:23:12 +0000

    now working

--- WebServer/application.py
+++ WebServer/application.py
@@ -34,8 +34,8 @@ def index():
     }
     return render_template('index.html', **templateData)
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+#if __name__ == '__main__':
+#    app.run(host='127.0.0.1', port=8080, debug=True)
 
 
 # [END gae_python37_app]
